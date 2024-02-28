00ba984dc211ab986ef39e64e3f8586845703057
HanadAhmedM <hanadahmed1414@gmail.com>
2024-02-28 13:35:02 +0100

    Testing for Searchview

---
+++ Recept/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Recept
+//
+//  Created by Hanad.Ahmed on 2024-02-28.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
