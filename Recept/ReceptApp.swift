77b5c8044fa4199085b47b49ae5b4c9f3717145b
Abdulrahman <a14azrak@gmail.com>
2024-02-28 08:33:59 +0100

    Initial Commit

---
+++ Recept/ReceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceptApp.swift
+//  Recept
+//
+//  Created by Abdulrahman.Alazrak on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct ReceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
