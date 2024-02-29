c84e1414be854dafd4ff636c55fd0daeef153fdf
HanadAhmedM <hanadahmed1414@gmail.com>
2024-02-29 17:05:23 +0100

    Create favoirite site Design

---
+++ Recept/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  Recept
+//
+//  Created by Hanad.Ahmed on 2024-02-29.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
